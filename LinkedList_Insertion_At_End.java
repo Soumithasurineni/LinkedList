3363fa5df070c4653c8cef69e12f06e2c509e48e
Soumithasurineni <97505689+Soumithasurineni@users.noreply.github.com>
2024-10-05 14:08:24 +0530

    Create LinkedList_Insertion_At_End.java

---
+++ LinkedList_Insertion_At_End.java
@@ -0,0 +1,32 @@
+/*
+Given the head of a Singly Linked List and a value x, insert that value x at the end of the LinkedList and return the modified Linked List.
+*/
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+  */
+class Solution {
+    // Function to insert a node at the end of the linked list.
+    Node insertAtEnd(Node head, int x) {
+        // code here
+        Node node=new Node(x);
+        Node temp=head;
+        if(head==null)
+        {
+            return node;
+        }
+        while(temp.next!=null)
+        {
+            temp=temp.next;
+        }
+        temp.next=node;
+        return head;
+        
+    }
+}
